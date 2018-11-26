f4fe5067767f289c9b97bad89b0cc802adf0f095
barbara-tavares <32822314+barbara-tavares@users.noreply.github.com>
2018-11-26 10:30:09 -0200

    primeiro commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,34 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
+import { AtendenteProvider, Atendente } from '../../providers/atendente/atendente';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage { 
+	model: Atendente;
 
-  constructor(public navCtrl: NavController) {
+	constructor(public navCtrl: NavController, public navParams: NavParams,
+    private toast: ToastController, private atendenteProvider: AtendenteProvider) { 
 
-  }
+			this.model = new Atendente();
+		}
 
+	ionViewDidEnter() {
+		console.log('ionViewDidLoad HomePage');
+	}
+	
+	Menu() {
+		this.navCtrl.push('MenuPage');
+	}
+	
+	getAdmin() {
+		this.atendenteProvider.getAtendente(1)
+		  .then((result: Atendente) => {
+			this.model = result;
+		  });
+	}
+
+    
 }
